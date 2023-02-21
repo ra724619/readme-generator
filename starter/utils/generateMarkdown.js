f6e14dadf6f92b3eca1c9e6312108384ccfb5cdf
ra724619@gmail.com <raymond131415@Tses-MacBook-Pro.local>
2023-02-21 15:25:53 +0000

    code push to GitHub

--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -2,6 +2,53 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contribution](#contribution)
+  * [Test](#test)
+  * [Credit](#credit)
+  * [Questions](#questions)
+
+  ## Description
+
+  ${data.description}
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## License
+
+  ![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)
+    
+  This application is covered by the ${data.license} license.
+
+  ## Contribution
+
+  ${data.contribution}
+
+  ## Test
+
+  ${data.test}
+
+  ## Credit
+
+  ${data.credit}
+
+  ## Questions
+  
+  My GitHub username: ${data.github}
+
+  My GitHub link: (https://github.com/${data.github})
+  
+  My Contact email: ${data.email}
 `;
 }
 
